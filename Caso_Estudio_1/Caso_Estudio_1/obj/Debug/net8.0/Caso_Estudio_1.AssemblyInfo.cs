56d8d4d4752075245890d4fc28f7a2621eeaf6dd
URANO000 <kkonigin1100@gmail.com>
2025-10-22 18:34:26 -0600

    Correcting a {

--- Caso_Estudio_1/Caso_Estudio_1/obj/Debug/net8.0/Caso_Estudio_1.AssemblyInfo.cs
+++ Caso_Estudio_1/Caso_Estudio_1/obj/Debug/net8.0/Caso_Estudio_1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Caso_Estudio_1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+34dcbd9fb5dacdf08842c977ebd997a617ef09dc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+97916210a54820b5ee0a6e84541a47acff0d9f97")]
 [assembly: System.Reflection.AssemblyProductAttribute("Caso_Estudio_1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Caso_Estudio_1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
