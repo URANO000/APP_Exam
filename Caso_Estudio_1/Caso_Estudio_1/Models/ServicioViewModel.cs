00dc899c9de397855906a148704e3f04cec4050b
Isacnb23 <157438923+Isacnb23@users.noreply.github.com>
2025-10-22 19:12:19 -0600

    Calse servicioViewModel

---
+++ Caso_Estudio_1/Caso_Estudio_1/Models/ServicioViewModel.cs
@@ -0,0 +1,21 @@
+﻿namespace Caso_Estudio_1.Models
+{
+    public class ServicioViewModel
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public int Especialidad { get; set; }
+        public string Especialista { get; set; }
+        public decimal Monto { get; set; }
+        public string Clinica { get; set; }
+        public decimal IVA { get; set; }
+
+        public string EspecialidadTexto => Especialidad switch
+        {
+            1 => "Medicina General",
+            2 => "Imagenología",
+            3 => "Microbiología",
+            _ => "N/D"
+        };
+    }
+}
