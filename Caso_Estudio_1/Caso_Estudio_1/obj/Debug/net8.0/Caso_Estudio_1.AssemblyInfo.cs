aac54d41d5823b1be50495d6cb9ea412e6e6b91b
Isacnb23 <157438923+Isacnb23@users.noreply.github.com>
2025-10-22 19:22:32 -0600

    vistas

--- Caso_Estudio_1/Caso_Estudio_1/obj/Debug/net8.0/Caso_Estudio_1.AssemblyInfo.cs
+++ Caso_Estudio_1/Caso_Estudio_1/obj/Debug/net8.0/Caso_Estudio_1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Caso_Estudio_1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+00dc899c9de397855906a148704e3f04cec4050b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d6758a1341127f0e44aa2d616aaf9aa578d7e2c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Caso_Estudio_1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Caso_Estudio_1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
