1587412246b83128b92bb201d72d7315fb048982
URANO000 <kkonigin1100@gmail.com>
2025-10-22 19:22:55 -0600

    Controller done for services

--- Caso_Estudio_1/Caso_Estudio_1/Controllers/ServiciosController.cs
+++ Caso_Estudio_1/Caso_Estudio_1/Controllers/ServiciosController.cs
@@ -3,6 +3,7 @@ using Caso_Estudio_1.Models;
 using Caso_Estudio_1.Models.Entities;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 
 namespace Caso_Estudio_1.Controllers
@@ -42,7 +43,80 @@ namespace Caso_Estudio_1.Controllers
             await dbContext.Servicios.AddAsync(servicios);
             //Save the changes
             await dbContext.SaveChangesAsync();
-            return RedirectToAction("List, Categorias");
+            return RedirectToAction("List, Servicios");
         }
+
+        //LIST
+        [HttpGet]
+        public async Task<IActionResult> List()
+        {
+            var servicios = await dbContext.Servicios.ToListAsync();
+            return View(servicios);
+        }
+        //EDIT
+        [HttpGet]
+        public async Task<IActionResult> Edit(int id)
+        {
+            var servicios = await dbContext.Servicios.FindAsync(id);
+            return View(servicios);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(Servicios viewModel)
+        {
+            var servicios = await dbContext.Servicios.FindAsync(viewModel.Id);
+
+            //Checking if exists
+            if(servicios != null)
+            {
+                servicios.Nombre = viewModel.Nombre;
+                servicios.Descripcion = viewModel.Descripcion;
+                servicios.Monto = viewModel.Monto;
+                servicios.IVA = viewModel.IVA;
+                servicios.Especialidad = viewModel.Especialidad;
+                servicios.Especialista = viewModel.Especialista;
+                servicios.Clinica = viewModel.Clinica;
+                servicios.FechaDeRegistro = DateTime.Now;
+                servicios.Estado = viewModel.Estado;
+
+                await dbContext.SaveChangesAsync();
+            }
+
+            return RedirectToAction("List", "Servicios");
+        }
+
+        //DELETE
+        [HttpGet]
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var servicios = await dbContext.Servicios.FirstOrDefaultAsync(x => x.Id == id);
+            return View(servicios);
+
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var servicios= await dbContext.Servicios.FindAsync(id);
+
+            if (servicios != null)
+            {
+                dbContext.Servicios.Remove(servicios);
+
+                //Finally save changes to the db
+                await dbContext.SaveChangesAsync();
+            }
+
+            //Now I can redirect to list!
+            return RedirectToAction("List", "Servicios");
+        }
+
+
+
     }
 }
--- Caso_Estudio_1/Caso_Estudio_1/obj/Debug/net8.0/Caso_Estudio_1.AssemblyInfo.cs
+++ Caso_Estudio_1/Caso_Estudio_1/obj/Debug/net8.0/Caso_Estudio_1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Caso_Estudio_1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+33d758ae8ed778eb94f425fc2d135182992e5978")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6cb1e8257c19033ce41a6ee019a23ab0a0a827ac")]
 [assembly: System.Reflection.AssemblyProductAttribute("Caso_Estudio_1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Caso_Estudio_1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
