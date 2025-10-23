9e26eef03c1d1f5f40b680346765196aa72347ed
aguilar1x <aaguilar1x@gmail.com>
2025-10-22 19:05:57 -0600

    feat: Add view Models AddCita and CitaDetails

---
+++ Caso_Estudio_1/Caso_Estudio_1/Models/AddCitaViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Caso_Estudio_1.Models
+{
+    public class AddCitaViewModel
+    {
+        public int IdServicio { get; set; }
+        public string NombreServicio { get; set; }
+        public string EspecialidadTexto { get; set; }
+        public string Especialista { get; set; }
+        public decimal Monto { get; set; }
+        public decimal IVA { get; set; }
+
+        [Required, StringLength(150)] public string NombreDeLaPersona { get; set; }
+        [Required, StringLength(30)] public string Identificacion { get; set; }
+        [Required, StringLength(10)] public string Telefono { get; set; }
+        [Required, EmailAddress, StringLength(50)] public string Correo { get; set; }
+        [Required] public DateTime FechaNacimiento { get; set; }
+        [Required, StringLength(200)] public string Direccion { get; set; }
+        [Required] public DateTime FechaDeLaCita { get; set; }
+
+        public decimal MontoTotal => Monto + (Monto * (IVA / 100m));
+    }
+}
+
---
+++ Caso_Estudio_1/Caso_Estudio_1/Models/CitaDetailsViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Caso_Estudio_1.Models
+{
+    public class CitaDetailsViewModel
+    {
+        public int Id { get; set; }
+        public string NombreDeLaPersona { get; set; }
+        public string Telefono { get; set; }
+        public string Correo { get; set; }
+        public string Identificacion { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+        public string Direccion { get; set; }
+        public string NombreServicio { get; set; }
+        public string EspecialidadTexto { get; set; }
+        public string Especialista { get; set; }
+        public decimal MontoTotal { get; set; }
+        public DateTime FechaDeLaCita { get; set; }
+        public DateTime FechaDeRegistro { get; set; }
+        public string Clinica { get; set; }
+    }
+}
